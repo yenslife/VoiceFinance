2326275d4a7ca7eefbd93c30fdce88e65910228f
yenslife <77geo5rge6@gmail.com>
2024-07-09 10:14:25 +0800

    Update prompt

--- backend/prompts.py
+++ backend/prompts.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from rich import print
 
 today = datetime.today()
 
@@ -15,6 +16,28 @@ def information_extraction(text):
     """
     提取給定的文字中的日期、金額、地點等訊息，並且使用 json 格式回傳，給 prompt 範例
     """
+    dates = {
+        "今天": today.strftime("%Y-%m-%d"),
+        "剛剛": today.strftime("%Y-%m-%d"),
+        "剛才": today.strftime("%Y-%m-%d"),
+        "昨天": (today - timedelta(days=1)).strftime("%Y-%m-%d"),
+        "3天前": (today - timedelta(days=3)).strftime("%Y-%m-%d"),
+        "上禮拜": (today - timedelta(weeks=1)).strftime("%Y-%m-%d"),
+        "一週前": (today - timedelta(weeks=1)).strftime("%Y-%m-%d"),
+        "兩週前": (today - timedelta(weeks=2)).strftime("%Y-%m-%d"),
+        "一個月前": (today - timedelta(weeks=4)).strftime("%Y-%m-%d"),
+        "兩個月前": (today - timedelta(weeks=8)).strftime("%Y-%m-%d"),
+    }
+    weekday_names = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]    
+    for i in range(0, 7):
+        dates[weekday_names[i]] = (today - timedelta(days=today.weekday() - i)).strftime("%Y-%m-%d")    
+    last_weekday_names_0 = ["上週一", "上週二", "上週三", "上週四", "上週五", "上週六", "上週日"]
+    last_weekday_names_1 = ["上禮拜一", "上禮拜二", "上禮拜三", "上禮拜四", "上禮拜五", "上禮拜六", "上禮拜日"]
+    for i in range(0, 7):
+        dates[last_weekday_names_0[i]] = (today - timedelta(days=today.weekday() - i + 7)).strftime("%Y-%m-%d")
+        dates[last_weekday_names_1[i]] = (today - timedelta(days=today.weekday() - i + 7)).strftime("%Y-%m-%d")
+    print(dates)
+    nl = "\n"
     prompt = f"""請提取以下文字中的日期、金額、地點等訊息，並回傳結果：
 '''{text}'''
 使用 json 格式回傳提取的結果，例如：
@@ -41,16 +64,7 @@ def information_extraction(text):
 }}
 
 當出現以下日期時，請回傳對應的日期：
-今天：{today.strftime("%Y-%m-%d")}
-剛剛：{{today.strftime("%Y-%m-%d")}}
-剛才：{{today.strftime("%Y-%m-%d")}}
-昨天：{(today - timedelta(days=1)).strftime("%Y-%m-%d")}
-3天前：{(today - timedelta(days=3)).strftime("%Y-%m-%d")}
-上禮拜：{(today - timedelta(weeks=1)).strftime("%Y-%m-%d")}
-一週前：{(today - timedelta(weeks=1)).strftime("%Y-%m-%d")}
-兩週前：{(today - timedelta(weeks=2)).strftime("%Y-%m-%d")}
-一個月前：{(today - timedelta(weeks=4)).strftime("%Y-%m-%d")}
-兩個月前：{(today - timedelta(weeks=8)).strftime("%Y-%m-%d")}
+{str(dates).replace("'", "").replace(", ", nl).replace("{", "").replace("}", "")}
 
 ###金額資訊###
 一萬塊：10000
