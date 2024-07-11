9602675b6ca109313d8e645e525083b6fa77b670
yenslife <77geo5rge6@gmail.com>
2024-07-11 18:17:45 +0800

    refactor(search): delete button
    use button instead of container
    to delete the item

--- frontend/custom_controls.py
+++ frontend/custom_controls.py
@@ -15,9 +15,10 @@ class ItemCard(ft.Container):
         self.padding = ft.padding.all(5)
 
 class Item(ft.ResponsiveRow):
-    def __init__(self, name, amount, location, date, create_at, id, note):
+    def __init__(self, name, amount, location, date, create_at, id, note, page: ft.Page = None, search_func=None):
         super().__init__()
-        self.screen_size_setting = {"sm": 1.7, "md": 1.7, "xl": 1.7}
+        self.page = page
+        self.screen_size_setting = {"sm": 0.85, "md": 0.85, "xl": 0.85}
         self.name_text_card = ItemCard(name)
         self.name_edit_card = ItemCard(content=ft.TextField(name, width=100))
         self.amount_text_card = ItemCard(amount)
@@ -32,10 +33,22 @@ class Item(ft.ResponsiveRow):
         self.id = id,
         self.edit_button = ft.IconButton(icon=ft.icons.EDIT, on_click=self.edit)
         self.save_button = ft.IconButton(icon=ft.icons.SAVE, on_click=self.save)
-        self.edit_button_container = ft.Container(self.edit_button, col=self.screen_size_setting,padding=ft.padding.all(10))
-        self.save_button_container = ft.Container(self.save_button, col=self.screen_size_setting,padding=ft.padding.all(10))
+        self.edit_button_container = ft.Container(self.edit_button, col=self.screen_size_setting)
+        self.save_button_container = ft.Container(self.save_button, col=self.screen_size_setting)
+        self.delte_button = ft.IconButton(icon=ft.icons.DELETE, on_click=self.delete)
+        self.delete_button_container = ft.Container(self.delte_button, col=self.screen_size_setting)
         self.alignment = ft.MainAxisAlignment.CENTER
         self.vertical_alignment = ft.CrossAxisAlignment.CENTER
+        self.dlg_modal = ft.AlertDialog(
+            modal=True,
+            title=ft.Text("Delete Item"),
+            content=ft.Text("Are you sure to delete this item?"),
+            actions=[
+                ft.TextButton("Cancel", on_click=self.handleClose),
+                ft.TextButton("Delete", on_click=self.handleDelete)
+            ],
+        )
+        self.search_func = search_func
         self.controls = [
             # ft.Checkbox(),
             self.name_text_card,
@@ -45,6 +58,7 @@ class Item(ft.ResponsiveRow):
             self.note_text_card,
             self.create_at_text_card,
             self.edit_button_container,
+            self.delete_button_container,
         ]
 
     def edit(self, e):
@@ -92,6 +106,34 @@ class Item(ft.ResponsiveRow):
         )
         print(response.json())
         self.update()
+    
+    def handleClose(self, e):
+        self.dlg_modal.open = False
+        self.page.update()
+        print("close")
+    
+    def handleDelete(self, e):
+        print("delete")
+        self.dlg_modal.open = False
+        # delete
+        try:
+            print(self.id)
+            response = requests.delete(API_URL + "/items/" + str(self.id[0]))
+        except Exception as e:
+            print(e)
+        if response.status_code == 200:
+            print("delete success")
+        else:
+            print("delete failed")
+        self.search_func(None)
+
+    def delete(self, e):
+        # show delete check
+        print("delete")
+        self.page.dialog = self.dlg_modal
+        self.dlg_modal.open = True
+        self.page.update()
+
 class AppBar(ft.AppBar):
     def __init__(self, title="Voice Finance 你的語音記帳系統", page: ft.Page = None):
         super().__init__()
--- frontend/pages/search_page.py
+++ frontend/pages/search_page.py
@@ -11,46 +11,6 @@ def search_page(page: ft.Page):
     appbar = AppBar(page=page)
     item_list_view = ft.ListView([], height=370)
 
-    def handleClose(e):
-        dlg_modal.open = False
-        page.update()
-        print("close")
-    
-    def handleDelete(e):
-        print("delete")
-        dlg_modal.open = False
-        # delete
-        try:
-            response = requests.delete(API_URL + "/items/" + str(select_target_id))
-        except Exception as e:
-            print(e)
-        if response.status_code == 200:
-            print("delete success")
-        else:
-            print("delete failed")
-        search(None)
-        page.update
-        
-    dlg_modal = ft.AlertDialog(
-        modal=True,
-        title=ft.Text("Delete Item"),
-        content=ft.Text("Are you sure to delete this item?"),
-        actions=[
-            ft.TextButton("Cancel", on_click=handleClose),
-            ft.TextButton("Delete", on_click=handleDelete)
-        ],
-    )
-
-    select_target_id = None
-    def item_onclick(e):
-        nonlocal select_target_id
-        # show delete check
-        item_id = e.control.content.id[0]
-        page.dialog = dlg_modal
-        dlg_modal.open = True
-        select_target_id = item_id
-        page.update()
-
     def search(e):
         nonlocal item_list_view
         query = search_bar.value
@@ -65,13 +25,14 @@ def search_page(page: ft.Page):
                             date=item['date_'], 
                             create_at=item['create_at'], 
                             id=item['id'],
-                            note=item['note'])
+                            note=item['note'],
+                            page=page,
+                            search_func=search)
             items.append(ft.Container(item_obj, 
                                       border=ft.border.all(1),
                                       border_radius=ft.border_radius.all(10),
                                       padding=ft.padding.all(1),
-                                      margin=ft.margin.all(10),
-                                      on_click=item_onclick,))
+                                      margin=ft.margin.all(10),))
         item_list_view.controls.extend(items)
         page.update()
 
